e56c28e5342aa55fe9d969c1ad9a70764b174596
CaRuaLeS <caruales97@gmail.com>
2019-12-12 16:07:37 +0100

    cursores añadidos y particula de movimiento de unidades

--- Assets/Materials/Cursores_Moline/Cursores.cs
+++ Assets/Materials/Cursores_Moline/Cursores.cs
@@ -5,102 +5,92 @@ using UnityEngine.UI;
 
 public class Cursores : MonoBehaviour
 {
-    /*
-    public void Spawn(Vector3 position)
-    {
-        Instantiate(cursorAnim).transform.position = position;
-    }
-    */
-    //Custom Cursor Setup
+	Units_Selection selected;
 
-    // [SerializeField]
-    public Texture2D cursorBattle;
-    public Texture2D cursorAtack;
-    public Texture2D cursorDefense;
-    public Texture2D cursorSlot;
-    public Texture2D mainCursor;
-    public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
-    // public GameObject cursorAnim;
-    bool collider;
+    [SerializeField] Texture2D cursorBattle;
+    [SerializeField] Texture2D cursorAtack;
+    [SerializeField] Texture2D cursorDefense;
+    [SerializeField] Texture2D cursorSlot;
+    [SerializeField] Texture2D mainCursor;
 
+    [SerializeField] CursorMode cursorMode = CursorMode.Auto;
+    [SerializeField] Vector2 hotSpot = Vector2.zero;
+
+	[SerializeField] GameObject movePointParticle;
+	ParticleSystem pointToMoveParticle;
+
+	GameObject mouseOnTop;
   
 
     void Start()
     {
+		pointToMoveParticle = movePointParticle.GetComponent<ParticleSystem>(); ;
+		selected = Units_Selection.selectionInstance;
+
         Cursor.SetCursor(mainCursor, Vector2.zero, CursorMode.Auto);
     }
-
-   
-
    
     void Update()
     {
-        Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit2;
-        // OnMouseEnter();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-        /*
-        Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit2;
+		if (Physics.Raycast(ray, out hit, 400))
+		{
+			mouseOnTop = hit.collider.gameObject;
+		}
 
+		if (selected.GetSelected().Count > 0 && Input.GetMouseButtonDown(1) && hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+		{
+			pointToMoveParticle.Simulate(0.0f, true, true);
+			movePointParticle.transform.position = new Vector3(hit.point.x, hit.point.y + 1, hit.point.z);
+			pointToMoveParticle.Play();
+
+
+		}
+
+		OnMouseExit();
+		OnMouseEnter();
+		/*
         if (Physics.Raycast(ray2, out hit2, Mathf.Infinity))
         {
-            print(hit2.collider.gameObject.tag);
-
-            
-
-            /*
             if (Input.GetKeyDown(KeyCode.Mouse1))
             {
                 Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
-
                 Vector3 ad = new Vector3(worldPoint.x, worldPoint.y, cursorAnim.transform.position.z);
-
                 Spawn(ad);
-            }
-          */
-
-
-
-        // }
-
-    }
-    void OnMouseEnter(){
-        Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit2;
-        if (Physics.Raycast(ray2, out hit2, Mathf.Infinity))
-        {
-            
-                if (hit2.collider.gameObject.tag == "SB_Slots")
-                {
-                
-                    Cursor.SetCursor(cursorSlot, Vector2.zero, CursorMode.Auto);
-                
-                
-                }
-                else Cursor.SetCursor(mainCursor, Vector2.zero, CursorMode.Auto);
-                if (hit2.collider.gameObject.tag == "SB_Defense")
-                {
-                    Cursor.SetCursor(cursorDefense, Vector2.zero, CursorMode.Auto);
-                }
-                else Cursor.SetCursor(mainCursor, Vector2.zero, CursorMode.Auto);
-                if (hit2.collider.gameObject.tag == "SB_Attack")
-                {
-                    Cursor.SetCursor(cursorAtack, Vector2.zero, CursorMode.Auto);
-                }
-                else Cursor.SetCursor(mainCursor, Vector2.zero, CursorMode.Auto);
-
+            }        
         }
+		*/
+	}
+
+    void OnMouseEnter(){
            
-       
-    }
+		if (mouseOnTop.tag == "SB_Slots")
+		{		
+		    Cursor.SetCursor(cursorSlot, Vector2.zero, CursorMode.Auto);			
+		}
+
+		if (mouseOnTop.tag == "SB_Defense")
+		{
+		    Cursor.SetCursor(cursorDefense, Vector2.zero, CursorMode.Auto);
+		}
+
+		if (mouseOnTop.tag == "SB_Attack")
+		{
+		    Cursor.SetCursor(cursorAtack, Vector2.zero, CursorMode.Auto);
+		}
+
+		if (mouseOnTop.transform.parent.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+		{
+			Cursor.SetCursor(cursorBattle, Vector2.zero, CursorMode.Auto);
+		}
+
+
+	}
         void OnMouseExit()
         {
-
-
             Cursor.SetCursor(mainCursor, Vector2.zero, CursorMode.Auto);
-
         }
 
     }
