df7aff5e4a133b9df0e33c192ad7faa47c5e9237
CaRuaLeS <caruales97@gmail.com>
2020-01-20 20:33:18 +0100

    Arreglando cagada de amenooo

---
+++ Assets/Scripts/Mapa_3/EfectoTexto.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class EfectoTexto : MonoBehaviour
+{
+
+    public float delay;
+
+    public string textoEntero;
+
+    private string textoActual = "";
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void MostrarTexto ()
+    {
+
+        StartCoroutine(Texto());
+    }
+
+    IEnumerator Texto ()
+    {
+        for (int i = 0; i < textoEntero.Length; i++)
+        {
+            textoActual = textoEntero.Substring(0, i);
+            this.GetComponent<Text>().text = textoActual;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
---
+++ Assets/Scripts/Mapa_3/Pergamino.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pergamino : MonoBehaviour
+{
+    public GameObject canvasPergamino;
+    public GameObject botónPergamino;
+    public PruebaCámara camaraSequencia;
+
+    //public PruebaCámara scriptCámara;
+    void Start()
+    {
+        camaraSequencia = GameObject.FindObjectOfType(typeof(PruebaCámara)) as PruebaCámara;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    public void Siguiente ()
+    {
+        canvasPergamino.SetActive(false);
+
+        camaraSequencia.SecuenciaDos();
+    }
+}
