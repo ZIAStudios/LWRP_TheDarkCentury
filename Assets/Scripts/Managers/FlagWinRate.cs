b2b64d3d5f8ff0241b1b75243fa9f28e4cd2d782
CaRuaLeS <caruales97@gmail.com>
2020-01-26 14:26:48 +0100

    opcion de victoria cuando capturas las 3 banderas implementar en código FLAGWINRATE

---
+++ Assets/Scripts/Managers/FlagWinRate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagWinRate : MonoBehaviour
+{
+    [SerializeField] float height = 50f;
+
+    public LayerMask flagLayer;
+
+    [SerializeField] Collider[] flagArray;
+
+
+
+    void Update()
+    {
+
+        OverlapFlag();
+
+
+    }
+
+    void OverlapFlag()
+    {
+
+        flagArray = Physics.OverlapBox(gameObject.transform.position, new Vector3(transform.localScale.x, transform.localScale.y + height, transform.localScale.z) / 2, Quaternion.identity, flagLayer);
+
+        for (int i = 0; i < flagArray.Length; i++)
+        {
+            if (flagArray[0].gameObject.GetComponent<Flag>().flagState == Zone.STATE.Player && flagArray[1].gameObject.GetComponent<Flag>().flagState == Zone.STATE.Player && flagArray[2].gameObject.GetComponent<Flag>().flagState == Zone.STATE.Player)
+            {
+                //PONER AQUI QUE SE QUIERA QUE PASE CUANDO CAPTURE LAS 3 BANDERAS
+                print("esto esta");
+            }
+
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(gameObject.transform.position, new Vector3(transform.localScale.x, transform.localScale.y + height, transform.localScale.z));
+    }
+}
